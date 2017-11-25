796f13acc4ff389aa4f9a8e5f96042e31b058ae5
Alex Shah <alexkshah@gmail.com>
2017-11-24 21:13:38 -0500

    t2t might work

--- shah-prj/working/script.sh
+++ shah-prj/working/script.sh
@@ -6,13 +6,15 @@
 # You can easily swap between them (and add new ones).
 t2t-trainer --registry_help
 
+HERE="~/git/MSCS/shah-prj/working/"
+
 PROBLEM=translate_enfr_wmt32k
 MODEL=transformer
 HPARAMS=transformer_base_single_gpu
 
-DATA_DIR=$HOME/data
-TMP_DIR=/tmp/t2t_datagen
-TRAIN_DIR=$HOME/train/$PROBLEM/$MODEL-$HPARAMS
+DATA_DIR=data/
+TMP_DIR=temp/
+TRAIN_DIR=train/$PROBLEM/$MODEL-$HPARAMS/
 
 mkdir -p $DATA_DIR $TMP_DIR $TRAIN_DIR
 
