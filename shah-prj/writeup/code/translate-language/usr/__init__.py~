bd5cce525b7089f35c31f2b1afde1b663c89433b
Alex Shah <alexkshah@gmail.com>
2017-12-04 11:39:32 -0500

    remove in class files, restructure prj, start writeup

---
+++ shah-prj/writeup/code/translate-language/usr/__init__.py~
@@ -0,0 +1,2 @@
+# Alex Shah Init
+from . import shah_en_es
---
+++ shah-prj/writeup/code/translate-language/usr/shah_en_es.py
@@ -0,0 +1,73 @@
+# coding=utf-8
+## Alex Shah
+## Purpose: To define a new problem set in tensor2tensor
+
+"""Data generators for translation data-sets."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import tarfile
+
+# Dependency imports
+
+from tensor2tensor.data_generators import generator_utils
+from tensor2tensor.data_generators import problem
+from tensor2tensor.data_generators import text_encoder
+from tensor2tensor.data_generators import translate
+from tensor2tensor.utils import registry
+
+from tensor2tensor.models import transformer
+
+import tensorflow as tf
+
+FLAGS = tf.flags.FLAGS
+
+# End-of-sentence marker.
+EOS = text_encoder.EOS_ID
+@registry.register_hparams
+def transformer_shah_hparams():
+  hparams = transformer.transformer_base()
+  hparams.hidden_size = 1024
+  
+@registry.register_problem
+class shah_en_es(translate.TranslateProblem):
+  """Problem spec for Shah EnEs Translator."""
+
+  @property
+  def targeted_vocab_size(self):
+    return 15000
+
+  @property
+  def vocab_name(self):
+    return "vocab.shah_en_es"
+
+  def feature_encoders(self, data_dir):
+    vocab_filename = os.path.join(data_dir, self.vocab_file)
+    encoder = text_encoder.TokenTextEncoder(vocab_filename, replace_oov="UNK")
+    return {"inputs": encoder, "targets": encoder}
+
+  def generator(self, data_dir, tmp_dir, train):
+    """Token generator Shah En Es Translator."""
+    dataset_path = ("es.txt"
+                    if train else "en.txt")
+    train_path = _get_wmt_ende_bpe_dataset(tmp_dir, dataset_path)
+    token_tmp_path = os.path.join(tmp_dir, self.vocab_file)
+    token_path = os.path.join(data_dir, self.vocab_file)
+    tf.gfile.Copy(token_tmp_path, token_path, overwrite=True)
+    with tf.gfile.GFile(token_path, mode="a") as f:
+      f.write("UNK\n")  # Add UNK to the vocab.
+    token_vocab = text_encoder.TokenTextEncoder(token_path, replace_oov="UNK")
+    return translate.token_generator(train_path + ".en", train_path + ".es",
+                                     token_vocab, EOS)
+
+  @property
+  def input_space_id(self):
+    return problem.SpaceID.SHAH_EN_TOK
+
+  @property
+  def target_space_id(self):
+    return problem.SpaceID.SHAH_ES_TOK
+
